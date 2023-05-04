5ff8e2ccda8518fdef0a3395d02fce6ebcc59d1e
Narayanan Murugan <86243034+m-narayanan22@users.noreply.github.com>
2023-05-04 12:55:59 +0530

    Update plots.py

--- plots.py
+++ plots.py
@@ -27,36 +27,32 @@ def app(cc_df):
     st.subheader("Visualisation Selector")
 
     # Add a multiselect in the sidebar with label 'Select the charts or plots:'
-    # and pass the remaining 3 plot types as a tuple i.e. ('Histogram', 'Box Plot', 'Correlation Heatmap').
+    # and pass the remaining 3 plot types as a tuple i.e. ('Box Plot', 'Correlation Heatmap').
     # Store the current value of this widget in a variable 'plot_types'.
-    plot_types = st.multiselect("Select charts or plots:", ('Histogram', 'Box Plot', 'Correlation Heatmap'))
-
-    # Display box plot using the 'matplotlib.pyplot' module and the 'st.pyplot()' function.
-    if 'Histogram' in plot_types:
-        st.subheader("Histogram")
-        columns = st.selectbox("Select the column to create its histogram",
-                                      ('carwidth', 'enginesize', 'horsepower'))
-        # Note: Histogram is generally created for continous values not for discrete values.
-        plt.figure(figsize = (12, 6))
-        plt.title(f"Histogram for {columns}")
-        plt.hist(car_df[columns], bins = 'sturges', edgecolor = 'black')
-        st.pyplot()
+    plot_types = st.multiselect("Select charts or plots:", ('Count Plot', 'Correlation Heatmap'))
+   
 
     # Create box plot using the 'seaborn' module and the 'st.pyplot()' function.
-    if 'Box Plot' in plot_types:
-        st.subheader("Box Plot")
+    if 'Count Plot' in plot_types:
+        st.subheader("Count Plot")
         columns = st.selectbox("Select the column to create its box plot",
-                                      ('carwidth', 'enginesize', 'horsepower'))
+                                      ('EDUCATION', 'MARRIAGE', 'SEX'))
         plt.figure(figsize = (12, 2))
         plt.title(f"Box plot for {columns}")
-        sns.boxplot(car_df[columns])
+        ax=sns.countplot(data=cc_df, x=columns, hue="Default")
+        for label in ax.containers:
+            ax.bar_label(label)
+        plt.xticks([0,1], labels=["male", "female"])
+        plt.title("Defaulters based on Gender")
+        plt.show()
+        #sns.countplot(cc_df[columns], hue=)
         st.pyplot()
 
     # Display correlation heatmap using the 'seaborn' module and the 'st.pyplot()' function.
     if 'Correlation Heatmap' in plot_types:
         st.subheader("Correlation Heatmap")
         plt.figure(figsize = (8, 5))
-        ax = sns.heatmap(car_df.corr(), annot = True) # Creating an object of seaborn axis and storing it in 'ax' variable
+        ax = sns.heatmap(cc_df.corr(), annot = True) # Creating an object of seaborn axis and storing it in 'ax' variable
         bottom, top = ax.get_ylim() # Getting the top and bottom margin limits.
         ax.set_ylim(bottom + 0.5, top - 0.5) # Increasing the bottom and decreasing the bottom margins respectively.
         st.pyplot()
